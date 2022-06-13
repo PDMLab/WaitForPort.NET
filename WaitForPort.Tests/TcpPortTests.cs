d318850c97b105cbea720aea4fbd49b31007315b
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-06-14 00:46:12 +0200

    feat: wait for tcp port

---
+++ WaitForPort.Tests/TcpPortTests.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Xunit;
+using static WaitForPort.Ports;
+
+namespace WaitForPortTests;
+
+public class TcpPortTests
+{
+  [Fact]
+  public async Task ShouldWaitForTcpPort()
+  {
+    var service = new TestcontainersBuilder<TestcontainersContainer>()
+      .WithImage("postgres:14")
+      .WithEnvironment(
+        "POSTGRES_PASSWORD",
+        "p455w02d"
+      )
+      .WithPortBinding(
+        5432,
+        5432
+      )
+      .Build();
+
+    await service.StartAsync();
+    await WaitForTcpPort(
+      5432,
+      10000
+    );
+    await service.StopAsync();
+  }
+
+  [Fact]
+  public async Task ShouldThrowOnUnavailableTcpPort()
+  {
+    var exception = await Assert.ThrowsAsync<ApplicationException>(
+      () => WaitForTcpPort(
+        5432,
+        10000
+      )
+    );
+
+    Assert.Equal(
+      "Timeout waiting for endpoint 127.0.0.1:5432",
+      exception.Message
+    );
+  }
+}
---
+++ WaitForPort/Ports.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WaitForPort;
+
+public class Ports
+{
+  public static async Task WaitForTcpPort(
+    int port,
+    int timeout = int.MaxValue
+  )
+  {
+    if (port <= 0)
+    {
+      throw new ArgumentException($"{nameof(timeout)} must be greater than zero.");
+    }
+    
+    if (timeout < 0)
+    {
+      throw new ArgumentException($"{nameof(timeout)} must be greater or equal to zero.");
+    }
+    WaitForPort(port, timeout);
+  }
+
+  private static void WaitForPort(
+    int port,
+    long timeoutMilliseconds
+  )
+  {
+    var endpoint = new IPEndPoint(
+      IPAddress.Parse("127.0.0.1"),
+      port
+    );
+    using (var socket = new Socket(
+             SocketType.Stream,
+             ProtocolType.Tcp
+           ))
+    {
+      var millisecondsToWait = (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) + timeoutMilliseconds;
+      while (true)
+        try
+        {
+          socket.Connect(endpoint);
+          break;
+        }
+        catch (Exception ex)
+        {
+          Thread.Sleep(1000);
+          var now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+          if (now >= millisecondsToWait)
+            throw new ApplicationException(
+              $"Timeout waiting for endpoint {endpoint.Address}:{endpoint.Port}",
+              ex
+            );
+        }
+    }
+  }
+}
